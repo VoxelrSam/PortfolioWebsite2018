2fb329226e1d6d612a2ca7d57041a07a1465eb62
VoxelrSam <samingram22@gmail.com>
2018-09-01 17:48:08 -0400

    Create Initial Landing Page and Refactor

--- scripts/main.js
+++ scripts/main.js
@@ -41,6 +41,79 @@ function loaded () {
 	
 	// animates in the header and footer after the loading screen has finished bouncing out
 	$('#loading-cover').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-		$("#headerContainer").addClass("animated slideInDown");
+        typeText("landing-introduction", "Hi, I'm Sam", function () {
+            setTimeout(() => {
+                typeText("landing-description", "I'm a ", startDescriptionSwap);
+            }, 500);
+        });
+
+        $("#landing-scroll-indicator").addClass("animated bounce").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            $("#landing-scroll-indicator").removeClass("animated bounce");
+        });
+
+        setInterval(() => {
+            $("#landing-scroll-indicator").addClass("animated bounce").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+                $("#landing-scroll-indicator").removeClass("animated bounce");
+            });
+        }, 3000);
 	});
+}
+
+function typeText (id, text, callback) {
+    const element = document.getElementById(id);
+
+    if (text.length === 0) {
+        element.innerHTML = element.innerHTML.substr(0, element.innerHTML.length - 1);
+
+        if (callback)
+            callback();
+    } else {
+        setTimeout(function () {
+            if (element.innerHTML.charAt(element.innerHTML.length - 1) === "_")
+                element.innerHTML = element.innerHTML.substr(0, element.innerHTML.length - 1);
+
+            element.innerHTML += text.charAt(0) + "_";
+
+            text = text.substr(1, text.length);
+
+            typeText(id, text, callback);
+        }, 25 + Math.random() * 50);
+    }
+}
+
+function deleteText (id, amount, callback) {
+    const element = document.getElementById(id);
+
+    if (amount === 0) {
+        if (callback)
+            callback();
+    } else {
+        setTimeout(function () {
+            element.innerHTML = element.innerHTML.substr(0, element.innerHTML.length - 2) + "_";
+            deleteText(id, amount - 1, callback);
+        }, 50);
+    }
+}
+
+function startDescriptionSwap () {
+    const descriptions = [
+        "Developer",
+        "Musician",
+        "Creator",
+        "Computer Scientist"
+    ];
+
+    let index = 0;
+
+    typeText("landing-description", descriptions[index] + "_");
+
+    setInterval(() => {
+        deleteText("landing-description", descriptions[index].length, () => {
+            index = (index + 1) % descriptions.length;
+
+            setTimeout(() => {
+                typeText("landing-description", descriptions[index] + "_");
+            }, 250);
+        });
+    }, 3000);
 }
